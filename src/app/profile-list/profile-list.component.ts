f7643a87e0523675518e3605b225e59b1dd8071a
Kayyam sai supraj <kayyamsaisupraj@Kayyams-MacBook-Pro.local>
2020-07-21 18:51:02 +0530

    Committed

---
+++ src/app/profile-list/profile-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import{FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { Subscription } from 'rxjs';
+import {ProfileServiceService}  from '/Users/kayyamsaisupraj/Documents/UItypescript/src/app/profile-service/profile-service.service'
+
+@Component({
+  selector: 'app-profile-list',
+  templateUrl: './profile-list.component.html',
+  styleUrls: ['./profile-list.component.scss']
+})
+export class ProfileListComponent implements OnInit {
+
+  title: string;
+  userData:  any;
+  user: any
+
+
+  constructor( private search  : ProfileServiceService) {   }
+      
+  onsearch(){
+     
+    this.search.getUsers(this.title).subscribe((result:any) => {
+      this.userData = result; })
+
+  }
+  
+  ngOnInit(): void {
+  }
+
+
+}
---
+++ src/app/profile-service/profile-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileServiceService {
+
+  constructor(private http: HttpClient) { }
+     
+   getUsers(user :string ){
+         
+        return this.http.get("https://api.github.com/search/users?q="+ user);
+
+   }
+  
+    
+
+}
